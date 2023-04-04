b1803ed38b60ea08b1ea3c2a65a2a9a9278b0530
GLADYS <mungaiglado106@gmail.com>
2023-04-04 13:58:15 +0300

    inital commit

--- src/Home.js
+++ src/Home.js
@@ -3,31 +3,61 @@ import BlogList from './BlogList';
 const Home = () => {
     
 
-const [blogs, setBlogs] = useState([
-   { title: 'Welcome to my new blog', body: 'lorem ipsum...', author: 'mario', id: 1},
-    { title: 'Web party!', body: 'lorem ipsum...', author: 'yoshi', id: 2},
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'yoshi', id: 3}
-]);  //useState hook
+const [blogs, setBlogs] = useState(null)
+     //useState hook gives us access to the state and a function to update the state
 
+const [isPending, setIsPending] = useState(true);
+
+const [error, setError] = useState(null);
     // function to delete a blog
-const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id);
-setBlogs(newBlogs);
-}
+// const handleDelete = (id) => {
+//     const newBlogs = blogs.filter(blog => blog.id !== id);
+// setBlogs(newBlogs);
+// }
       
 
-//useEffect hook
+//useEffect hook to fetch data from the server
 useEffect(() => {
-    console.log('use effect ran');
-    console.log(blogs);
-})        
+    setTimeout(() => {
+    fetch('http://localhost:8000/blogs')
+    .then(response => {
+        console.log(response);
+        if (!response.ok) {
+            throw Error('could not fetch the data for that resource')
+
+        }
+        return response.json()})
+
+        .then(data => {
+          
+            setBlogs(data);
+            setIsPending(false);
+            
+        })
+        // catch error and log it to the console
+   .catch(err => {
+         // auto catches network / connection error
+        setError(err.message);
+        setIsPending(false);
+        
+   })
+        
+    }, 1000); //timeout of 1 second
+    
+}, []);        
+
+// if error is true, display the error message
+
     //passing the blogs array as a prop to the BlogList component
     return (
    <div className="home">
+{isPending && <div>Loading...</div>}
 
 
-<BlogList blogs={blogs} title="All blogs!" handleDelete={handleDelete} /> 
- <BlogList blogs={blogs} title="Mario's blogs!" />  
+{error && <div>{error}</div>}
+
+{blogs && <BlogList blogs={blogs} title="All blogs!" />} 
+ 
     </div>
             
     ); 
